03f51073eb135b57218c2a04e17955ee0fbaf975
chillM <cxmhill@gmail.com>
2018-07-21 11:14:20 -0500

    Initial Deck Setup

---
+++ SoulHorizons/Assets/Scripts/Combat/Deck/Deck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+
+	public int deckSize = 30;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
