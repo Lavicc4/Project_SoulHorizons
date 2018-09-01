04a3fa6655f6e6fcc13817c69457d4b95b8ecb7a
Unknown <cxmhill@gmail.com>
2018-09-01 11:21:18 -0500

    Minor Deck Edits

--- SoulHorizons/Assets/Scripts/Combat/Deck/Deck.cs
+++ SoulHorizons/Assets/Scripts/Combat/Deck/Deck.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+struct Card
+{
+	int num;
+	string name;
+}
+
 /// <summary>
 /// Contains the deck and discard piles. Handles shuffling and drawing.
 /// </summary>
 public class Deck : MonoBehaviour {
 
 	public int deckSize = 30;
-
+	List<Card> deck;
 
 	void Start () {
+		deck = new List<Card>();
+	}
+
+	/// <summary>
+	/// Loads the list of cards in the deck from a save file
+	/// </summary>
+	void LoadDeckList() {
 		
+	}
+
+	/// <summary>
+	/// Shuffles the deck. By default this does not include the hand.
+	/// </summary>
+	/// <param name="shuffleHand"></param>
+	void Shuffle(bool shuffleHand = false) {
+
 	}
 	
-	void Update () {
-		
-	}
 }
