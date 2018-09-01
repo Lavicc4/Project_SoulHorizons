902632d932ec4b3a1a007e25ffa3bb915ffce544
Phuong <phuonghuynh1550@gmail.com>
2018-09-01 16:19:19 -0500

    Added New Mechanics
    Soul Transform/Reward Message

---
+++ SoulHorizons/Assets/Scripts/Combat/scr_statemanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scr_statemanager : MonoBehaviour {
+
+    public Text rewardMessage;
+    bool endCombat = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            //Debug.Log("NO ENEMIES");
+            rewardMessage.enabled = true;
+            endCombat = true;
+        }
+        if (endCombat)
+        {
+            if (Input.GetKey(KeyCode.V))
+            {
+                Debug.Log("Switching Scenes");
+                SceneManager.LoadScene("sn_overworldtest");
+            }
+        }
+	}
+}
