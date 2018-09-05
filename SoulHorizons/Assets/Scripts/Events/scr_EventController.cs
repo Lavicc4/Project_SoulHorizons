195e6cad56f28f04f3db945f9613cc6f87324118
Cameron Countryman <cmc151030@utdallas.edu>
2018-09-05 16:55:48 -0500

    Working on Event Generation
    trying to get non repeating events to generate randomly.  Still WIP

---
+++ SoulHorizons/Assets/Scripts/Events/scr_EventController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scr_EventController : MonoBehaviour {
+
+	public Button[] buttons = new Button[10]; 
+	public int numberOfActiveEvents;
+	 
+
+	void Start () {
+
+
+
+		//for loop to turn off all buttons
+		for (int i = 0; i < buttons.Length; i++) {
+			buttons [i].gameObject.SetActive (false);
+		}
+
+
+	}
+	
+
+	void Update () {
+
+
+		if (Input.GetKeyDown (KeyCode.J)) {	
+			CycleEvents (); 
+		}
+		if (Input.GetKeyDown (KeyCode.E)) {	
+			Debug.Log(TestingGeneration (0, 10,3)); 
+		}
+
+
+
+	}
+
+	void CycleEvents(){
+
+
+		for (int i = 0; i < buttons.Length; i++) {
+			buttons [i].gameObject.SetActive (false);
+		}
+
+		int[] temp = new int[numberOfActiveEvents];
+
+		temp = TestingGeneration (0, buttons.Length, numberOfActiveEvents);
+		for(int i = 0; i < temp.Length;i++){
+			
+		}
+
+	}
+
+		
+	int[] TestingGeneration(int min, int max, int length){
+		int[] testArray = new int[length];
+		HashSet<int> h = new HashSet<int>();
+
+		while (h.Count < testArray.Length){
+			h.Add(Random.Range(min, max));
+			h.CopyTo(testArray);
+		}
+		return testArray;
+
+
+	}
+}
