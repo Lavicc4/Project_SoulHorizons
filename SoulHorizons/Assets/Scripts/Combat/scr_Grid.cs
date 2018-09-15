485868edeb8cb817993eaaa9b22bcdcff7bd3a6f
huynhp724 <phuonghuynh1550@gmail.com>
2018-09-15 14:50:26 -0500

    More Bug Fixes

--- SoulHorizons/Assets/Scripts/Combat/scr_Grid.cs
+++ SoulHorizons/Assets/Scripts/Combat/scr_Grid.cs
@@ -14,30 +14,17 @@ public class scr_Grid : MonoBehaviour{
     private void Start()
     {
         grid = new GameObject[xsize, ysize];
+        scr_PlayerMovement player_coords = GameObject.FindGameObjectWithTag("Player").GetComponent<scr_PlayerMovement>();
         for (int i = 0; i < xsize; i++)
         {
             for (int j = 0; j < ysize; j++)
             {
                 Debug.Log(i + " " + j);
-                GameObject tiletoadd = (GameObject)Instantiate(tile, new Vector3(i + center_x, -j + center_y, 0), Quaternion.identity);
+                GameObject tiletoadd = (GameObject)Instantiate(tile, new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x + i, GameObject.FindGameObjectWithTag("Player").transform.position.y - j, 0), Quaternion.identity);
                 grid[i, j] = tiletoadd;
             }
         }
     }
-
-    /*public void Initialize()
-    {
-        scr_Tile[,] grid = new scr_Tile[xsize, ysize];
-        for (int i = 0; i < xsize; i++)
-        {
-            for (int j = 0; j < ysize; j++)
-            {
-                //GameObject tile = (GameObject)Instantiate(); ;
-                grid[i, j] = new scr_Tile(i, j);
-            }
-        }
-        Debug.Log("CENTER: " + grid[0, 0].center.x);
-    }*/
 
     // Update is called once per frame
     void Update () {
