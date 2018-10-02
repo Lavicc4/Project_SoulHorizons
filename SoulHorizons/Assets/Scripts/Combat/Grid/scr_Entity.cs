7620730551e874fdb0ae74997ed532f6d82e7992
huynhp724 <phuonghuynh1550@gmail.com>
2018-10-01 19:44:30 -0500

    Invincibility Frames

--- SoulHorizons/Assets/Scripts/Combat/Grid/scr_Entity.cs
+++ SoulHorizons/Assets/Scripts/Combat/Grid/scr_Entity.cs
@@ -18,16 +18,30 @@ public class scr_Entity : MonoBehaviour
     public scr_EntityAI _ai;
     public scr_Tile.Territory entityTerritory;
     public SpriteRenderer spr;
-    public float lerpSpeed; 
+    Color baseColor;
+    public float lerpSpeed;
+    public bool has_iframes;
+    bool invincible = false;
+    public float invulnTime;
+    float invulnCounter = 0f;
 
     public void Start()
     {
-      
+        baseColor = spr.color;
     }
     public void Update()
     {
         _ai.UpdateAI();
-        transform.position = Vector3.Lerp(transform.position, scr_Grid.GridController.GetWorldLocation(_gridPos.x, _gridPos.y), (lerpSpeed*Time.deltaTime));              
+        transform.position = Vector3.Lerp(transform.position, scr_Grid.GridController.GetWorldLocation(_gridPos.x, _gridPos.y), (lerpSpeed*Time.deltaTime));       
+        //Counts down iframes
+        if(invulnCounter > 0)
+        {
+            invulnCounter -= Time.deltaTime;
+        }
+        else
+        {
+            setInvincible(false);
+        }
     }
 
 
@@ -55,8 +69,16 @@ public class scr_Entity : MonoBehaviour
         Attack atk = scr_AttackController.attackController.MoveIntoAttackCheck(_gridPos);
         if(atk != null)
         {
-            //Debug.Log("I'M HIT");
-            HitByAttack(atk); 
+            if (!invincible)
+            {
+                Debug.Log("I'M HIT");
+                HitByAttack(atk);
+                if (has_iframes)
+                {
+                    //Activate invincibility frames
+                    setInvincible(true);
+                }
+            }
         }
         
     }
@@ -69,6 +91,30 @@ public class scr_Entity : MonoBehaviour
           
         }
     }
+
+    public bool isInvincible()
+    {
+        return invincible;
+    }
+
+    public void setInvincible(bool inv)
+    {
+        invincible = inv;
+        if (inv)
+        {
+            Debug.Log("I'M INVINCIBLE");
+            invulnCounter = invulnTime;
+            spr.color = Color.gray;
+        }
+        else
+        {
+            Debug.Log("NOT INVINCIBLE");
+            invulnCounter = 0f;
+            invincible = false;
+            spr.color = baseColor;
+        }
+    }
+   
 }
 [System.Serializable]
 public class Health{
