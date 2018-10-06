7d0b59dd33b0305610fba21a7ac39622d7b54841
Colin Hill <cxmhill@gmail.com>
2018-10-06 14:44:05 -0500

    SoulTransform System Start and EZCameraShake import
    Also changed blaster sprite offset

---
+++ SoulHorizons/Assets/Scripts/Combat/Soul Transform/scr_SoulManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Update the UI, take in user input, and manage information on the available soul transforms
+/// </summary>
+public class scr_SoulManager : MonoBehaviour {
+
+    public List<SoulTransform> soulTransforms = new List<SoulTransform>();
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Replace the default list of transforms with passed-in arguments
+    /// </summary>
+    /// <param name="transforms"></param>
+    public void SetSoulTransforms(params SoulTransform[] transforms)
+    {
+        soulTransforms = new List<SoulTransform>(transforms.Length);
+
+
+    }
+
+    /// <summary>
+    /// Get user input
+    /// </summary>
+    private void UserInput()
+    {
+
+    }
+}
