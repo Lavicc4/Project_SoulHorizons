44990874e30433a1c295d03c5ac38ca2a22d3c9c
huynhp724 <phuonghuynh1550@gmail.com>
2018-10-09 06:20:08 -0500

    Auto stash before merge of "Combat" and "origin/Combat"

--- SoulHorizons/Assets/Scripts/Combat/Deck/Cards/scr_HardenSoul.cs
+++ SoulHorizons/Assets/Scripts/Combat/Deck/Cards/scr_HardenSoul.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Cards/HardenSoul")]
 public class scr_HardenSoul : scr_Card {
-
+   
     public int Shield_hp;
     public override void Activate()
     {
---
+++ SoulHorizons/Assets/Scripts/Combat/Deck/Cards/scr_InnerStrength.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Cards/InnerStrength")]
+public class scr_InnerStrength : scr_Card {
+
+
+    public override void Activate()
+    {
+        ActivateEffects();
+
+        scr_Entity player = GameObject.FindGameObjectWithTag("Player").GetComponent<scr_Entity>();
+        
+
+    }
+
+    public override void StartCastingEffects()
+    {
+
+    }
+
+    protected override void ActivateEffects()
+    {
+        //put start effects here
+    }
+}
