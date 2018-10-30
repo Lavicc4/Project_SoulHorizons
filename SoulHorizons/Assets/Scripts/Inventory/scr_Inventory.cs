668d2c4f8d1734a51c5d8407eec14e7140cc71f6
huynhp724 <phuonghuynh1550@gmail.com>
2018-10-30 12:42:14 -0500

    Inventory WIP

---
+++ SoulHorizons/Assets/Scripts/Inventory/scr_Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class scr_Inventory{
+
+    public static int dustNum; //How much dust you have
+    public static List<KeyValuePair<string, int>> cardInv; //Your list of cards
+    public static List<KeyValuePair<string, int>> deckList; //Your deck list
+
+    
+}
