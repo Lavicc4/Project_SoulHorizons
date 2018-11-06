6998e035662a83d02dcd4b8cc1e224c575a65386
Cameron Countryman <cmc151030@utdallas.edu>
2018-11-06 12:05:42 -0600

    new Health bar

--- SoulHorizons/Assets/Scripts/Combat/Grid/scr_HealthBar.cs
+++ SoulHorizons/Assets/Scripts/Combat/Grid/scr_HealthBar.cs
@@ -6,15 +6,20 @@ public class scr_HealthBar : MonoBehaviour {
 
     float _health;
     float _maxHp;
-    public GameObject pivot; 
+    float _shield; 
+    public GameObject pivot;
+    public GameObject bluePivot; 
 
 	void Start () {
-        _health = GetComponentInParent<scr_Entity>()._health.hp; 
+        _health = GetComponentInParent<scr_Entity>()._health.hp;
+        _shield = GetComponentInParent<scr_Entity>()._health.shield; 
 	}
 	
 	void Update () {
         _health = GetComponentInParent<scr_Entity>()._health.hp;
         _maxHp = GetComponentInParent<scr_Entity>()._health.max_hp;
+        _shield = GetComponentInParent<scr_Entity>()._health.shield;
         pivot.transform.localScale = new Vector3(_health/_maxHp, 1,1);
+        bluePivot.transform.localScale = new Vector3(_shield / _maxHp,1,1);
     }
 }
